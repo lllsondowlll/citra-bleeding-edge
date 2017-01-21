f465bd18cef5c84156c1c5a8791ce688601c7a91
wwylele <wwylele@gmail.com>
2017-01-21 11:53:03 +0200

    InputCommon: add Keyboard

---
+++ src/input_common/main.h
@@ -0,0 +1,23 @@
+// Copyright 2017 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include "input_common/keyboard.h"
+
+namespace InputCommon {
+
+/// Initializes and registers all built-in input device factories.
+void Init();
+
+/// Unresisters all build-in input device factories and shut them down.
+void Shutdown();
+
+/// Gets the keyboard button device factory.
+Keyboard* GetKeyboard();
+
+/// Generates a serialized param package for creating a keyboard button device
+std::string GenerateKeyboardParam(int key_code);
+
+} // namespace InputCommon
